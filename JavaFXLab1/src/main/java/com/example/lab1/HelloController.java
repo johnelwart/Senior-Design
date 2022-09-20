3a1d2153b1f4a956dc4163189db6cccdd157b898
p-moya <pamela-moya@uiowa.edu>
2022-09-20 17:30:01 -0500

    JavaFx Pamela

---
+++ JavaFXLab1/src/main/java/com/example/lab1/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.lab1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label fDegrees;
+
+    @FXML
+    private Label cDegrees;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
