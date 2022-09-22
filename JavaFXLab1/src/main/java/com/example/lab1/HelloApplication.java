f90e47e0ad34c9c831931e433aeeb2f8c5601a58
p-moya <pamela-moya@uiowa.edu>
2022-09-22 13:23:12 -0500

    just in case

--- JavaFXLab1/src/main/java/com/example/lab1/HelloApplication.java
+++ JavaFXLab1/src/main/java/com/example/lab1/HelloApplication.java
@@ -1,8 +1,13 @@
 package com.example.lab1;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +18,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("lab1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         //stage.setMaximized(true);
+
         stage.setTitle("Binary Beasts Lab 1");
         stage.setScene(scene);
         stage.show();
