a56dabdcbe2bfb3dd28d9f065b520cdc6a563da9
John Elwart <johnelwart00@gmail.com>
2022-09-26 16:38:11 -0500

    Update Lab1.ino

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,6 +1,4 @@
-//www.diyusthad.com
 #include <LiquidCrystal.h>
-//#include <DS18B20.h> // git repo for library: https://github.com/matmunk/DS18B20/blob/master/README.md
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
@@ -12,10 +10,13 @@ const int rs = 12,
           d7 = 3;
 
 const long interval = 2000;
-unsigned long prevMS = 0;
+unsigned long prevMS = 0,
+              currMS;
+
+String degC = "Degrees C: ",
+       degF = "Degrees F: ";
           
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
 OneWire oneWire(2);
 DallasTemperature sensor(&oneWire);
 
@@ -23,21 +24,36 @@ void setup() {
   lcd.begin(16, 2);
   sensor.begin();
   pinMode(7, INPUT);
+
+  sensor.requestTemperatures();
+  degC = "Degrees C: " + String(sensor.getTempCByIndex(0));
+  degF = "Degrees F: " + String(sensor.getTempFByIndex(0));
 }
 
 void loop() {
   if (digitalRead(7) == HIGH){
-    unsigned long currMS = millis();
+    currMS = millis();
+
+     if (sensor.getTempCByIndex(0) == -127) {
+     printToLCD("     Error!     ", "Sensor not found");
+       
+     } else {
+      printToLCD(degC, degF);
+     }
 
     if (currMS - prevMS >= interval){
       prevMS = currMS;
       sensor.requestTemperatures();
-  
-      lcd.setCursor(0, 0);
-      lcd.print("Degrees C: " + String(sensor.getTempCByIndex(0)));
+
+      if (sensor.getTempCByIndex(0) == -127) {
+        printToLCD("     Error!     ", "Sensor not found");
+        
+      } else {
+        degC = "Degrees C: " + String(sensor.getTempCByIndex(0));
+        degF = "Degrees F: " + String(sensor.getTempFByIndex(0));
     
-      lcd.setCursor(0,1);
-      lcd.print("Degrees F: " + String(sensor.getTempFByIndex(0)));
+        printToLCD(degC, degF);
+      }
 
     }
     
@@ -45,4 +61,12 @@ void loop() {
     lcd.clear();
   }
   
+}
+
+void printToLCD(String str1, String str2) {
+  lcd.setCursor(0,0);
+  lcd.print(str1);
+
+  lcd.setCursor(0,1);
+  lcd.print(str2);
 }
