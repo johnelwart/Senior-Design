fdab76d5433a041f9fcdf989692bb35cfcf6f2ee
p-moya <pamela-moya@uiowa.edu>
2022-09-27 17:18:20 -0500

    trying to implement global variables

--- JavaFXLab1/src/main/java/com/example/lab1/Example.java
+++ JavaFXLab1/src/main/java/com/example/lab1/Example.java
@@ -1,25 +1,20 @@
 package com.example.lab1;
-
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-import java.net.URI;
-
 public class Example {
-    // Find your Account SID and Auth Token at twilio.com/console
-    // and set the environment variables. See http://twil.io/secure
-    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
-    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = TWILIO_ACCOUNT_SID;
+    public static final String AUTH_TOKEN = "xg";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-        Message message = Message.creator(
-                        new com.twilio.type.PhoneNumber("+15558675310"),
-                        new com.twilio.type.PhoneNumber("+15017122661"),
-                        "McAvoy or Stewart? These timelines can get so confusing.")
-                .setStatusCallback(URI.create("http://postb.in/1234abcd"))
-                .create();
+
+        // To, From
+        Message message = Message.creator(new PhoneNumber("+15632759872"),
+                new PhoneNumber("+13862604740"),
+                "I'm watching you...").create();
 
         System.out.println(message.getSid());
     }
