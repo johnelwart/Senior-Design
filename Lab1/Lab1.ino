55026d7fac71504771c6ebc348f856684b67d1ea
John Elwart <johnelwart00@gmail.com>
2022-09-29 15:29:15 -0500

    Update Lab1.ino

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -5,13 +5,6 @@
 const int rs = 12,
           en = 11, 
           d4 = 6, 
-          d5 = 5, #include <LiquidCrystal.h>
-#include <DallasTemperature.h>
-#include <OneWire.h>
-
-const int rs = 12,
-          en = 11, 
-          d4 = 6, 
           d5 = 5, 
           d6 = 4, 
           d7 = 3;
@@ -90,74 +83,4 @@ void getTemps() {
   sensor.setWaitForConversion(false);
   sensor.requestTemperatures();
   sensor.setWaitForConversion(true);
-}
-          d6 = 4, 
-          d7 = 3;
-
-const long interval = 2000;
-unsigned long prevMS = 0,
-              currMS;
-
-String degC = "Degrees C: ",
-       degF = "Degrees F: ";
-          
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-OneWire oneWire(2);
-DallasTemperature sensor(&oneWire);
-
-void setup() {
-  lcd.begin(16, 2);
-  sensor.begin();
-  pinMode(7, INPUT);
-  Serial.begin(9600);
-
-  sensor.requestTemperatures();
-  degC = "Degrees C: " + String(sensor.getTempCByIndex(0));
-  degF = "Degrees F: " + String(sensor.getTempFByIndex(0));
-}
-
-void loop() {
-
-  
-  if (digitalRead(7) == HIGH){
-    currMS = millis();
-
-     if (sensor.getTempCByIndex(0) == -127) {
-      printToLCD("     Error!     ", "Sensor not found");
-      
-     } else {
-      printToLCD(degC, degF);
-     }
-
-    if (currMS - prevMS >= interval){
-      prevMS = currMS;
-      sensor.requestTemperatures();
-
-      if (sensor.getTempCByIndex(0) == -127) {
-        printToLCD("     Error!     ", "Sensor not found");
-        Serial.print(String("-127\n"));
-        
-      } else {
-        degC = "Degrees C: " + String(sensor.getTempCByIndex(0));
-        degF = "Degrees F: " + String(sensor.getTempFByIndex(0));
-    
-        printToLCD(degC, degF);
-        
-        Serial.print(String(sensor.getTempCByIndex(0)) + "\n");
-      }
-
-    }
-    
-  } else {
-    lcd.clear();
-  }
-  
-}
-
-void printToLCD(String str1, String str2) {
-  lcd.setCursor(0,0);
-  lcd.print(str1);
-
-  lcd.setCursor(0,1);
-  lcd.print(str2);
 }
