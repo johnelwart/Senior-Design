96a6467fecc1dfdc342fedc17f04fe3a6966ff0a
John Elwart <johnelwart00@gmail.com>
2022-09-30 15:04:30 -0500

    Added comments to the two arduino files

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -3,92 +3,84 @@
 #include <OneWire.h>
 #include <SoftwareSerial.h>
 
-const int rs = 12,
-          en = 11, 
-          d4 = 6, 
+const int rs = 12,            // Arduino pin number for the register select pin on the LCD
+          en = 11,            // Arduino pin number for the enable pin on the LCD
+          d4 = 6,             // Arduino pins for the data lines on the LCD
           d5 = 5, 
           d6 = 4, 
           d7 = 3;
 
-const long interval = 500;
+const long interval = 500;    // Half second constant
            
-unsigned long prevMS = 0,
-              currMS;
+unsigned long prevMS = 0,     // Previous millisecond value that the code executed at
+              currMS;         // Stores the current milliseconds since the program began execution using the millis() function
 
-String degC = "Degrees C: ",
+String degC = "Degrees C: ",  // Strings that get printed out to the LCD. At this point the temperatures are not taken yet.
        degF = "Degrees F: ";
           
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-OneWire oneWire(2);
-DallasTemperature sensor(&oneWire);
-SoftwareSerial BTSerial(9, 10);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);   // Creates an LCD object to control the LCD with the library methods
+OneWire oneWire(2);                          // Library used to communicate with one wire temperature sensors
+DallasTemperature sensor(&oneWire);          // Creates a temperature sensor object to use the library methods in the code
+SoftwareSerial BTSerial(9, 10);              // Creates a serial object to write and read data from the HC-06 bluetooth module using pin 9 for the RX and pin 10 for the TX
 
 void setup() {
-  lcd.begin(16, 2);
-  sensor.begin();
-  pinMode(7, INPUT);
-  Serial.begin(9600);
+  lcd.begin(16, 2);        // Initializes the LCD and sets the dimensions of the display. 16 columns, 2 rows
+  sensor.begin();          // Initializes the temperature sensor
+  Serial.begin(9600);      // Sets the baud rate or data rate of the serial data transmission to 9600 bits per second
+  BTSerial.begin(9600);    // Sets the baud rate of the bluetooth transmission to 9600 bits per second
 
-  Serial.begin(9600);
-  Serial.println("Arduino with HC-06 is ready");
- 
-  // HC-06 default baud rate is 9600
-  BTSerial.begin(9600);  
-  Serial.println("BTserial started at 9600");
+  pinMode(7, INPUT);       // Configures pin 7 on the Arduino as an input for determining if the button is pressed
 
-  getTemps();
+  getTemps();              // Calls the helper function to request the temperatures from the DS18B20 asynchronously
   
-  degC = "Degrees C: " + String(sensor.getTempCByIndex(0));
-  degF = "Degrees F: " + String(sensor.getTempFByIndex(0));
+  degC = "Degrees C: " + String(sensor.getTempCByIndex(0));   // Adds the celsius reading from the sensor to the cooresponding string
+  degF = "Degrees F: " + String(sensor.getTempFByIndex(0));   // Adds the fahrenheit reading from the sensor to the cooresponding string
 }
 
 void loop() {
 
-  // Keep reading from HC-06 and send to Arduino Serial Monitor
-  while (BTSerial.available() > 0)
-    Serial.write(BTSerial.read());
- 
-  // Keep reading from Arduino Serial Monitor and send to HC-06
-  while (Serial.available() > 0)
-    BTSerial.write(Serial.read());
+  currMS = millis();     // Gets the number of milliseconds that have passed since the program began and stores it in currMS
 
-  currMS = millis();
-
+  // If the number of milliseconds since the last execution of the statement is greater or equal to 500 then execute the code again
   if (currMS - prevMS >= interval) {
-    prevMS = currMS;
+    prevMS = currMS;     // Store the current time as the new previous execution
     
-    getTemps();
-  
+    getTemps();          // Get the temperatures from the sensor asynchronously to increase speed and responsiveness
+
+    // Check if the button is pushed
     while (digitalRead(7) == HIGH) {
-      
+
+     // Check for an error code from the sensor
      if (sensor.getTempCByIndex(0) == -127) {
-       printToLCD("     Error!     ", "Sensor not found");
+       printToLCD("     Error!     ", "Sensor not found");    // Print an error message to the LCD using the helper function
          
-     } else {
+     } else {                     // Otherwise, print the previous data from the last time the LCD was active
        printToLCD(degC, degF);
      }
        
-     getTemps();
-    
+     getTemps();                  // Get the temperatures again to make sure they are accurate and up to date
+
+     // Check for an error code from the sensor
      if (sensor.getTempCByIndex(0) == -127) {
-       printToLCD("     Error!     ", "Sensor not found");
+       printToLCD("     Error!     ", "Sensor not found");          // Print the error message to the LCD
             
      } else {
-       degC = "Degrees C: " + String(sensor.getTempCByIndex(0));
-       degF = "Degrees F: " + String(sensor.getTempFByIndex(0));
+       degC = "Degrees C: " + String(sensor.getTempCByIndex(0));    // Generate a new celsius string to display to the LCD
+       degF = "Degrees F: " + String(sensor.getTempFByIndex(0));    // Generate a new fahrenheit string to display
         
-       printToLCD(degC, degF);
+       printToLCD(degC, degF);    // Print the new strings to the LCD using the helper function
 
-       BTSerial.print(String(sensor.getTempCByIndex(0)) + "\n");  
+       BTSerial.print(String(sensor.getTempCByIndex(0)) + "\n");    // Send the sensor data to the bluetooth module for transmission to the HC-05
      }   
     }
     
-    BTSerial.print(String(sensor.getTempCByIndex(0)) + "\n");  
-    lcd.clear();
+    BTSerial.print(String(sensor.getTempCByIndex(0)) + "\n");    // Sends the sensor data to the bluetooth module for transmission
+    lcd.clear();    // Clears the LCD
     
   }
 }
 
+// Helper function to simplify the code. This function takes two strings for each line on the LCD and prints them accordingly using setCursor
 void printToLCD(String str1, String str2) {
   lcd.setCursor(0,0);
   lcd.print(str1);
@@ -97,6 +89,7 @@ void printToLCD(String str1, String str2) {
   lcd.print(str2);
 }
 
+// Helper function that requests the temperatures from the sensors asynchronously and simplifies the code.
 void getTemps() {
   sensor.setWaitForConversion(false);
   sensor.requestTemperatures();
--- Lab1/ReceiverCode.ino
+++ Lab1/ReceiverCode.ino
@@ -1,51 +1,18 @@
-// Basic Bluetooth sketch HC-05_03_9600
-// Connect the HC-05 module and communicate using the serial monitor
-//
-// The HC-05 defaults to commincation mode when first powered on.
-// Needs to be placed in to AT mode
-// After a factory reset the default baud rate for communication mode is 38400
-//
-//
-//  Pins
-//  BT VCC to Arduino 5V out. 
-//  BT GND to GND
-//  BT RX to Arduino pin 3 (through a voltage divider)
-//  BT TX to Arduino pin 2 (no need voltage divider)
- 
- 
 #include <SoftwareSerial.h>
-SoftwareSerial BTserial(2, 3); // RX | TX
-// Connect the HC-05 TX to Arduino pin 2 RX. 
-// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+
+SoftwareSerial BTserial(2, 3);    // Sets up a serial object using pins 2 for the RX and 3 for the TX
+
+char c = ' ';                     // Holds one character of data that is received from the HC-06 on the other circuit
  
-char c = ' ';
- 
-void setup() 
-{
-    // start the serial communication with the host computer
-    Serial.begin(9600);
-    Serial.println("Arduino with HC-05 is ready");
- 
-    // start communication with the HC-05 using 9600
-    BTserial.begin(9600);  
-    Serial.println("BTserial started at 9600");
+void setup() {
+    Serial.begin(9600);      // Sets the baud rate of the serial communication to 9600 bits per second
+    BTserial.begin(9600);    // Sets the baud rate of the bluetooth communication to 9600 bits per second
 }
  
-void loop()
-{
- 
-     // Keep reading from HC-05 and send to Arduino Serial Monitor
-    if (BTserial.available())
-    {  
+void loop() {
+    // If the is data avaiable on the RX pin, read the data one character at a time to c and print it to the serial monitor
+    if (BTserial.available()) {  
         c = BTserial.read();
         Serial.write(c);
     }
- 
-    // Keep reading from Arduino Serial Monitor and send to HC-05
-    if (Serial.available())
-    {
-        c =  Serial.read();
-        BTserial.write(c);  
-    }
- 
 }
